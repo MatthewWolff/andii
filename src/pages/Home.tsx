6f590beffd1a02123a7fed37c72365492a6665ae
Matthew Wolff <mtwol@amazon.com>
2025-12-19 13:01:42 -0800

    Add Bootstrap containers with shadows to home page
    - Add title container with 'The Andizzle Zone'
    - Add menu container around navigation buttons
    - Include Bootstrap CSS for styling
    - Set proper z-index for containers above falling icons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,33 +6,62 @@ function Home() {
     return (
         <div className="home">
             <XmasImagePreloader />
-            <h1>:3</h1>
-            <nav className="menu">
-                <Link to="/xmas-2025/" className="menu-item">
-                    <span>Merry Christmas!</span>
-                    <span>🎄</span>
-                </Link>
-                <Link to="/sanrio-quiz/" className="menu-item">
-                    <span>Sanrio Quiz</span>
-                    <span>🎀</span>
-                </Link>
-                <Link to="/crossword/" className="menu-item">
-                    <span>The Crossword</span>
-                    <span>🧩</span>
-                </Link>
-                <Link to="/first-dates/" className="menu-item">
-                    <span>20 First Dates</span>
-                    <span>💕</span>
-                </Link>
-                <Link to="/chamchi-fan-art/" className="menu-item">
-                    <span>Chamchi Fan Art</span>
-                    <span>🎨</span>
-                </Link>
-                <Link to="/birthday/" className="menu-item">
-                    <span>Andii's Birthday</span>
-                    <span>🎉</span>
-                </Link>
-            </nav>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card shadow border-0 title-container mb-4">
+                            <div className="card-body text-center py-3">
+                                <h1 className="mb-0">The Andizzle Zone</h1>
+                            </div>
+                        </div>
+                        <div className="card shadow border-0 menu-container">
+                            <div className="card-body p-4">
+                                <nav className="menu">
+                                    <Link
+                                        to="/xmas-2025/"
+                                        className="menu-item"
+                                    >
+                                        <span>Merry Christmas!</span>
+                                        <span>🎄</span>
+                                    </Link>
+                                    <Link
+                                        to="/sanrio-quiz/"
+                                        className="menu-item"
+                                    >
+                                        <span>Sanrio Quiz</span>
+                                        <span>🎀</span>
+                                    </Link>
+                                    <Link
+                                        to="/crossword/"
+                                        className="menu-item"
+                                    >
+                                        <span>The Crossword</span>
+                                        <span>🧩</span>
+                                    </Link>
+                                    <Link
+                                        to="/first-dates/"
+                                        className="menu-item"
+                                    >
+                                        <span>20 First Dates</span>
+                                        <span>💕</span>
+                                    </Link>
+                                    <Link
+                                        to="/chamchi-fan-art/"
+                                        className="menu-item"
+                                    >
+                                        <span>Chamchi Fan Art</span>
+                                        <span>🎨</span>
+                                    </Link>
+                                    <Link to="/birthday/" className="menu-item">
+                                        <span>Andii's Birthday</span>
+                                        <span>🎉</span>
+                                    </Link>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
